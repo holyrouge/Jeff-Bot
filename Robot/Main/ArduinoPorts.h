4b3e0a664fbad8bdd4cf891cbea40febfe0973aa
andr1908m <31960992+andr1908m@users.noreply.github.com>
2017-10-24 17:15:34 -0400

    Add files via upload

---
+++ Robot/Main/ArduinoPorts.h
@@ -0,0 +1,24 @@
+//portlist
+#ifndef ArduinoPorts_h
+#define ArduinoPorts_h
+
+const int leftDirection = 2;
+const int leftSpeed = 4;
+
+const int rightDirection = 9;
+const int rightSpeed = 7;
+
+const int ultraSendPort = 1;
+const int ultraReceivePort = 1;
+
+const int infraSendPort = 1;
+const int ifraReceivePort = 1;
+
+const int colorSendPort = 1;
+const int colorReceivePort = 1;
+
+const int clawPort = 1;
+const int xBeePort = 1;
+
+#endif
+
---
+++ Robot/Subsystems/SubsystemFunctions.h
@@ -0,0 +1,15 @@
+#ifndef RobotDrive_h
+#define RobotDrive_h
+
+void moveForward(int speed);
+void moveBackward(int speed);
+void turnLeft(int speed);
+void turnRight(int speed);
+
+const int leftDirection = 2;
+const int leftSpeed = 4;
+
+const int rightDirection = 9;
+const int rightSpeed = 7;
+
+#endif
