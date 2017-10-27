cc47a27050d6155a0190d99da60b8819d8c11911
andr1908m <31960992+andr1908m@users.noreply.github.com>
2017-10-26 21:41:46 -0400

    Added functions for subsystems
    Added the functions that will be used for every "insert-subsystem-name-here".ino file

--- Robot/Subsystems/SubsystemFunctions.h
+++ Robot/Subsystems/SubsystemFunctions.h
@@ -1,15 +1,37 @@
-#ifndef RobotDrive_h
-#define RobotDrive_h
+#ifndef SubsystemFunctions_h
+#define SubsystemFunctions_h
+
+void stop();
 
 void moveForward(int speed);
+void moveForward(int speed, double time);
+void moveForwardFullSpeed(double time);
+
 void moveBackward(int speed);
+void moveBackward(int speed, double time);
+void moveBackwardFullSpeed(double time);
+
 void turnLeft(int speed);
 void turnRight(int speed);
+void turnLeft(int speed, double time);
+void turnRight(int speed, double time);
+void turnLeft(int speed, int degrees);
+void turnRight(int speed, int degrees);
 
-const int leftDirection = 2;
-const int leftSpeed = 4;
+void clawSetOpen();
+void clawSetClose();
+void clawSetDegrees();
 
-const int rightDirection = 9;
-const int rightSpeed = 7;
+void colorOpen();
+void colorClose();
+void colorGrab();
+
+void infraOn();
+void infraOff();
+void infraGrab();
+
+void ultraOn();
+void ultraOff();
+void ultraRead();
 
 #endif
