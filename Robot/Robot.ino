aa7cfb179d359de5cde773cd54fed43016147ef5
andr1908m <31960992+andr1908m@users.noreply.github.com>
2017-11-03 00:10:20 -0400

    Provided example code for testing functions
    Provided example code for testing moveBackward(int speed, int time) function;
    Hidden surprise at end of file ;D ;

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -1,33 +1,112 @@
 #include <Arduino.h>
-#include "Subsystems/RobotDrive.h"
+#include "Main/ArduinoPorts.h"
 #include "Subsystems/RobotDrive.ino"
 
-/*
-  Turns on an LED on for one second, then off for one second, repeatedly.
-*/
 
 void setup()
 {
 	Serial.begin(9600);
-
 
     pinMode(leftSpeed, OUTPUT);
     pinMode(rightDirection, OUTPUT);
     pinMode(rightSpeed, OUTPUT);
     pinMode(leftDirection, OUTPUT);
 
-
 	pinMode(13, OUTPUT);
 }
 
 void loop()
 {
-	Serial.println("Hello world!");
-
-    // moveBackward(255);
-	delay(1000);              // wait for a second
-	digitalWrite(13, HIGH);   // set the LED on
-	delay(1000);              // wait for a second
-	digitalWrite(13, LOW);    // set the LED off
+    moveBackward(50, 3000); //runs for three seconds
+    delay(3000); // waits for three seconds before looping :D
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// issa robot
