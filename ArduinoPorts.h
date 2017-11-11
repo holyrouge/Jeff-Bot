2fd26b2ac9bbfdf429c12d5b292f1a906ff2f9ef
andr1908m <31960992+andr1908m@users.noreply.github.com>
2017-11-10 21:10:43 -0500

    Defined variable for ports for ultrasonics...
    Defined variable for ports for ultrasonics;
    Updated ports;
    New ultraGrab();

---
+++ ArduinoPorts.h
@@ -0,0 +1,31 @@
+//portlist
+#ifndef ArduinoPorts_h
+#define ArduinoPorts_h
+
+const int leftDirection = 8;
+const int leftSpeed = 10;
+
+const int rightDirection = 7;
+const int rightSpeed = 9;
+
+
+const int ultraRightSendPort = 1;
+const int ultraRightReceivePort = 1;
+const int ultraLeftSendPort = 1;
+const int ultraLeftReceivePort = 1;
+const int ultraFrontSendPort = 2;
+const int ultraFrontReceivePort = 3;
+
+const int infraRightSendPort = 1;
+const int infraRightReceivePort = 1;
+const int infraLeftReceivePort = 1;
+const int infraLeftSendPort = 1;
+
+const int colorSendPort = 1;
+const int colorReceivePort = 1;
+
+const int clawPort = 1;
+const int xBeePort = 1;
+
+#endif
+
---
+++ SubsystemFunctions.h
@@ -0,0 +1,37 @@
+
+#ifndef SubsystemFunctions_h
+#define SubsystemFunctions_h
+
+//@RobotDrive
+void stop();
+
+void moveForward(int speed);
+void moveForward(int speed, double time);
+void moveForwardFullSpeed(double time);
+
+void moveBackward(int speed);
+void moveBackward(int speed, double time);
+void moveBackwardFullSpeed(double time);
+
+void turnLeft(int speed);
+void turnRight(int speed);
+void turnLeft(int speed, double time);
+void turnRight(int speed, double time);
+
+//@Servo
+//everything can be done with myservo.write() :: takes in the amount of degrees you want
+
+
+//@ColorSensor
+void ColorOpen();
+uint16_t * colorSet();
+
+//@InfraSensor
+void InfraOn();
+void infraGrab();
+
+//@UltraSensor
+void UltraOn();
+double ultraGrab(int sendPort, int receivePort);
+
+#endif
